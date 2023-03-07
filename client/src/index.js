73d973adc49cee8ecc2c4a01fc104c89368219f7
LTFYR <lutfiyariea@code.edu.az>
2023-03-07 15:22:07 +0400

    baseurl

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import DataProvider from "./redux/store";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://ltfyr-social-media.onrender.com/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
